994a7456180e37ca412f7e8dab74946b219e02c4
Collin Crites <collin.crites@gmail.com>
2025-11-16 08:47:58 -0800

    Black formatting.

--- python/src/analysis/sound_processor.py
+++ python/src/analysis/sound_processor.py
@@ -1,19 +1,19 @@
 import librosa
 
+
 class Synthesizer:
-    def __init__(self, file: str) -> object:
-        
+    def __init__(self, sound_data: str | bytes) -> object:
         """
         An object for manipulating sound clips.
         ---
-        file, str: The location of the sound file on the host system.
+        sound_data, str | bytes: Either the directory string to a file or a bytes object.
         """
 
-        self.sound_file = file
+        self.sound_data = sound_data
 
     def load_sound(self):
         """
         Loads the file into the object.
         """
 
-        self.sound = librosa.load(self.sound_file, sr=None)
+        self.sound = librosa.load(self.sound_data, sr=None)
